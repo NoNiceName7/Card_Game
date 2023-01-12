8fb616eddc8c5ce4919a6840e93ff6f47a0fc7e4
NoNiceName7 <121935358+NoNiceName7@users.noreply.github.com>
2023-01-12 14:21:31 +0200

    update of Player

---
+++ game_cards/CardGame.py
@@ -0,0 +1,5 @@
+from Card import Card
+#class of DeckOfCards
+
+
+
---
+++ game_cards/Player.py
@@ -0,0 +1,21 @@
+from Card import Card
+from DeckOfCards import *
+from random import choice
+#class of player
+class Player:
+    def __init__(self,name:str,num_cards:int=26):
+        self.name=name
+        self.num_cards=num_cards
+        self.hand=[]
+    def set_hand(self,deck:DeckOfCards):
+        for card in range(self.num_cards):
+            self.hand.append(deck.deal_one())
+    def get_card(self):
+        pick=choice(self.hand)
+        self.hand.remove(pick)
+        return pick
+    def add_card(self,card:Card):
+        self.hand.append(card)
+
+
+
