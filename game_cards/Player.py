510333ae826530140671eb672d36315bfcc86113
amitkrispin <akrispin50@gmail.com>
2023-01-12 16:53:49 +0200

    work in program

--- game_cards/Player.py
+++ game_cards/Player.py
@@ -1,20 +1,25 @@
 from Card import Card
 from DeckOfCards import *
 from random import choice
-#class of player
+
+
+# class of player
 class Player:
-    def __init__(self,name:str,num_cards:int=26):
-        self.name=name
-        self.num_cards=num_cards
-        self.hand=[]
-    def set_hand(self,deck:DeckOfCards):
+    def __init__(self, name: str, num_cards: int = 26):
+        self.name = name
+        self.num_cards = num_cards
+        self.hand = []
+
+    def set_hand(self, deck: DeckOfCards):
         for card in range(self.num_cards):
             self.hand.append(deck.deal_one())
+
     def get_card(self):
-        pick=choice(self.hand)
+        pick = choice(self.hand)
         self.hand.remove(pick)
         return pick
-    def add_card(self,card:Card):
+
+    def add_card(self, card: Card):
         self.hand.append(card)
 
     def __repr__(self):
